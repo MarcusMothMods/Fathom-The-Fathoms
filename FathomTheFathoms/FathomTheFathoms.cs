3fcfb98752ae14d630ef5e4a32831f556cd3c347
MarcusMothMods <marcusmoth8@gmail.com>
2026-06-04 02:12:41 -0700

    Add files via upload

---
+++ FathomTheFathoms/FathomTheFathoms.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using Winch.Core;
+
+namespace FathomTheFathoms
+{
+	public class FathomTheFathoms : MonoBehaviour
+	{
+		public void Awake()
+		{
+			WinchCore.Log.Debug($"{nameof(FathomTheFathoms)} has loaded!");
+		}
+	}
+}
---
+++ FathomTheFathoms/Loader.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace FathomTheFathoms
+{
+	public class Loader
+	{
+		/// <summary>
+		/// This method is run by Winch to initialize your mod
+		/// </summary>
+		public static void Initialize()
+		{
+			var gameObject = new GameObject(nameof(FathomTheFathoms));
+			gameObject.AddComponent<FathomTheFathoms>();
+			GameObject.DontDestroyOnLoad(gameObject);
+		}
+	}
+}
